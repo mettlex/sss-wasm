d05cfb35e8e6b134420b898a545ae02338c00826
Mettle X <56449790+mettlex@users.noreply.github.com>
2022-01-12 21:15:22 +0600

    convert to async fn and add types

--- lib.js
+++ lib.js
@@ -3,30 +3,53 @@ const Wrapper = require("./wrapper.js");
 let api = undefined;
 
 function load() {
-  if (api) return new Promise(resolve => resolve(api));
-  return new Promise(resolve => {
-    Wrapper().then(Module => {
+  if (api) return new Promise((resolve) => resolve(api));
+  return new Promise((resolve) => {
+    Wrapper().then((Module) => {
       api = {
         set: (d, p) => Module.HEAPU8.set(d, p),
         slice: (f, t) => Module.HEAPU8.slice(f, t),
-        getShareLen: Module.cwrap('get_share_len', 'number', []),
-        getMessageLen: Module.cwrap('get_message_len', 'number', []),
-        getKeyshareLen: Module.cwrap('get_keyshare_len', 'number', []),
-        getKeyLen: Module.cwrap('get_key_len', 'number', []),
-        createBuffer: Module.cwrap('create_buffer', 'number', ['number', 'number']),
-        destroyBuffer: Module.cwrap('destroy_buffer', '', ['number']),
-        createShares: Module.cwrap('create_shares', 'number', ['number', 'number', 'number']),
-        combineShares: Module.cwrap('combine_shares', 'number', ['number', 'number']),
-        createKeyshares: Module.cwrap('create_keyshares', 'number', ['number', 'number', 'number']),
-        combineKeyshares: Module.cwrap('combine_keyshares', 'number', ['number', 'number']),
+        getShareLen: Module.cwrap("get_share_len", "number", []),
+        getMessageLen: Module.cwrap("get_message_len", "number", []),
+        getKeyshareLen: Module.cwrap("get_keyshare_len", "number", []),
+        getKeyLen: Module.cwrap("get_key_len", "number", []),
+        createBuffer: Module.cwrap("create_buffer", "number", [
+          "number",
+          "number",
+        ]),
+        destroyBuffer: Module.cwrap("destroy_buffer", "", ["number"]),
+        createShares: Module.cwrap("create_shares", "number", [
+          "number",
+          "number",
+          "number",
+        ]),
+        combineShares: Module.cwrap("combine_shares", "number", [
+          "number",
+          "number",
+        ]),
+        createKeyshares: Module.cwrap("create_keyshares", "number", [
+          "number",
+          "number",
+          "number",
+        ]),
+        combineKeyshares: Module.cwrap("combine_keyshares", "number", [
+          "number",
+          "number",
+        ]),
       };
       resolve(api);
     });
   });
 }
 
-exports.createShares = function createShares(data, n, k) {
-  return load().then(api => {
+/**
+ * @param {Uint8Array} data
+ * @param {number} n
+ * @param {number} k
+ * @returns {Promise<Uint8Array[]>}
+ */
+exports.createShares = async (data, n, k) =>
+  load().then((api) => {
     const datap = api.createBuffer(data.length);
     api.set(data, datap);
     const sharep = api.createShares(datap, n, k);
@@ -39,10 +62,13 @@ exports.createShares = function createShares(data, n, k) {
     }
     return shares;
   });
-};
 
-exports.combineShares = function combineShares(shares) {
-  return load().then(api => {
+/**
+ * @param {Uint8Array[]} shares
+ * @returns {Promise<Uint8Array>}
+ */
+exports.combineShares = async (shares) =>
+  load().then((api) => {
     const input = api.createBuffer(api.getShareLen() * shares.length);
     for (let s in shares) {
       api.set(shares[s], input + s * api.getShareLen());
@@ -54,10 +80,9 @@ exports.combineShares = function combineShares(shares) {
 
     return api.slice(datap, datap + api.getMessageLen());
   });
-};
 
 exports.createKeyshares = function createKeyshares(key, n, k) {
-  return load().then(api => {
+  return load().then((api) => {
     const keyp = api.createBuffer(key.length);
     api.set(key, keyp);
     const sharep = api.createKeyshares(keyp, n, k);
@@ -70,10 +95,10 @@ exports.createKeyshares = function createKeyshares(key, n, k) {
     }
     return shares;
   });
-}
+};
 
 exports.combineKeyshares = function combineKeyshares(keyshares) {
-  return load().then(api => {
+  return load().then((api) => {
     const input = api.createBuffer(api.getKeyshareLen() * keyshares.length);
     for (let s in keyshares) {
       api.set(keyshares[s], input + s * api.getKeyshareLen());
@@ -84,4 +109,4 @@ exports.combineKeyshares = function combineKeyshares(keyshares) {
 
     return api.slice(keyp, keyp + api.getKeyLen());
   });
-}
+};
